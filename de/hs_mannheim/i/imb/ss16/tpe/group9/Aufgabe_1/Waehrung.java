63b11bcb779a84dec1f56b8406031c1f88d4dec7
student <student@192.168.101.22>
2016-03-16 15:47:27 +0100

    Erste Bearbeitung der Klasse Waehrung

---
+++ de/hs_mannheim/i/imb/ss16/tpe/group9/Aufgabe_1/Waehrung.java
@@ -0,0 +1,29 @@
+
+public class Waehrung {
+	private String name;
+	private String kuerzel;
+	private String kurs;
+	
+	public String toString(){
+		return name + " " + kuerzel + " " + kurs;
+	}
+	
+	public String getName(){
+		return name;
+	}
+	public String getKuerzel(){
+		return kuerzel;
+	}
+	public String getKurs(){
+		return kurs;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+		
+	}
+
+}
